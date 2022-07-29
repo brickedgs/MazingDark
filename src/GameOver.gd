f92ad18972475dd31f2cff697b333f0ec6faf81d
derickfelix <derickfelix@zoho.com>
2022-07-28 23:17:53 -0300

    feat: add splash, icon and create GameOver.gd

--- src/GameOver.gd
+++ src/GameOver.gd
@@ -3,6 +3,9 @@ extends Spatial
 onready var start_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/StartButton
 onready var quit_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/QuitButton
 onready var fader = $CanvasLayer/Fader
+onready var jump_scare_timer = $JumpScareTimer
+onready var jump_scare_sprite = $CanvasLayer/Fader/JumpScareSprite
+onready var jump_scare_audio = $JumpScareAudio
 
 export (PackedScene) var game_scene = null
 
@@ -10,6 +13,10 @@ func _ready():
 	start_button.connect("pressed", self, "start_button_pressed")
 	quit_button.connect("pressed", self, "quit_button_pressed")
 	fader.connect("fade_finished", self, "on_fade_finished")
+	
+	jump_scare_timer.connect("timeout", self, "on_jump_scare_timer_timeout")
+	jump_scare_timer.start()
+	jump_scare_sprite.visible = true
 
 func start_button_pressed():
 	fader.fade_out()
@@ -19,3 +26,7 @@ func quit_button_pressed():
 	
 func on_fade_finished():
 	get_tree().change_scene_to(game_scene)
+
+func on_jump_scare_timer_timeout():
+	jump_scare_sprite.visible = false
+	jump_scare_audio.stop()
